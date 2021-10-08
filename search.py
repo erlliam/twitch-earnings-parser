daeb0060dd9bed1b37182a90c5557f3635fad54a
Erlliam Mejia <tommejas@gmail.com>
2021-10-08 02:34:12 -0400

    Improve error message if user's revenue isn't found

--- search.py
+++ search.py
@@ -38,9 +38,17 @@ def get_user_id(username):
 
 
 def print_monthly_revenue(user_id):
-    revenues = subprocess.check_output(
-        ["rg", "-IN", "--color=never", user_id, "all_revenues"], encoding="UTF-8"
-    )
+    try:
+        revenues = subprocess.check_output(
+            ["rg", "-IN", "--color=never", user_id, "all_revenues"], encoding="UTF-8"
+        )
+    except subprocess.CalledProcessError as error:
+        if error.returncode == 1:
+            raise Exception(
+                "The user's revenue was not found in the data. This can happen if the user is banned."
+            )
+
+        raise error
 
     dates_and_money = []
     rows = revenues.splitlines()
